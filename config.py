3ed03773705a668076467de60ede7ab1aca3377b
sloth4413 <lazymather@gmail.com>
2016-03-03 15:28:47 +0800

    [init version]

---
+++ config.py
@@ -0,0 +1,9 @@
+SECRET_KEY = "never tell you"
+MONGODB_SETTINGS = {
+    'DB': 'cleanblog_db',
+    'host': '127.0.0.1'
+}
+# for chinese
+# BABEL_DEFAULT_LOCALE = "zh_Hans_CN"
+# for english
+BABEL_DEFAULT_LOCALE = "en"
